0e02f957c6472f62342577539676b8feae70aefd
Ethan Vizitei <ethan.vizitei@gmail.com>
2024-08-14 12:22:37 -0500

    list comprehensions and tuples

--- baby.hs
+++ baby.hs
@@ -11,3 +11,7 @@ combineList a b = a ++ b
 myCons a b = a : b
 
 atIndex list index = list !! index
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+triangleOfLength l = [ (a,b,c) | c <- [1..l], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == l]
