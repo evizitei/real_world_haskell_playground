e9ceb371ce01a584f7d01249ff6ecd01bab72ff6
Ethan Vizitei <ethan.vizitei@gmail.com>
2024-08-21 11:33:17 -0500

    make a little mydrop function using conditionals and recursion

---
+++ basic.hs
@@ -0,0 +1,6 @@
+-- https://book.realworldhaskell.org/read/types-and-functions.html
+add a b = a + b
+
+myDrop n xs = if n <= 0 || null xs
+              then xs
+              else myDrop ((-) n 1) (tail xs)
