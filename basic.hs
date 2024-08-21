a93f006e885b644cb85ebce76d3a8e0db1cdc3ce
Ethan Vizitei <ethan.vizitei@gmail.com>
2024-08-21 12:44:33 -0500

    helper functions for working through haskell expression evaluation

--- basic.hs
+++ basic.hs
@@ -1,6 +1,13 @@
 -- https://book.realworldhaskell.org/read/types-and-functions.html
 add a b = a + b
 
+myDrop :: Int -> [a] -> [a]
 myDrop n xs = if n <= 0 || null xs
               then xs
               else myDrop ((-) n 1) (tail xs)
+
+isOdd n = mod n 2 == 1
+
+lastButOne xs = if length xs <= 2
+                then head xs
+                else lastButOne (tail xs)
