9310aab8e86277c18e0b449752bba1bdd7d5bead
Ethan Vizitei <ethan.vizitei@gmail.com>
2024-08-28 12:48:12 -0500

    use pattern matching in function definitions

--- tree.hs
+++ tree.hs
@@ -4,3 +4,21 @@ data Tree a = Node a (Tree a) (Tree a)
 
 data WeakTree a = WeakNode a (Maybe (WeakTree a)) (Maybe (WeakTree a))
                 deriving (Show)
+
+nodesAreSame (Node a _ _) (Node b _ _)
+            | a == b = Just a
+nodesAreSame _ _ = Nothing
+
+safeSecond :: [a] -> Maybe a
+safeSecond [] = Nothing
+safeSecond xs = if null (tail xs)
+                then Nothing
+                else Just (head (tail xs))
+
+myDrop n xs = if n <= 0 || null xs
+                then xs
+                else myDrop (n - 1) (tail xs)
+
+tidyDrop n xs | n <= 0 = xs
+tidyDrop _ [] = []
+tidyDrop n (_:xs) = tidyDrop (n - 1) xs
