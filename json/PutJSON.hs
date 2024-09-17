e062850c03a23fcd83f837d0317a82ba22224906
Ethan Vizitei <ethan.vizitei@gmail.com>
2024-09-17 18:55:26 +0200

    put JSON

---
+++ json/PutJSON.hs
@@ -0,0 +1,26 @@
+module PutJSON (renderJVal) where
+
+import Data.List (intercalate)
+import Jason
+
+commify = intercalate ", "
+
+tricat :: String -> String -> String -> String
+tricat a b c = a ++ b ++ c
+
+renderJVal :: JValue -> String
+renderJVal (JString s) = show s
+renderJVal (JNumber s) = show s
+renderJVal (JBool True) = "t"
+renderJVal (JBool False) = "f"
+renderJVal JNull = "null"
+renderJVal (JObject o) = tricat "{" (pairs o) "}"
+    where pairs [] = ""
+          pairs ps = commify (map renderPair ps)
+          renderPair (k, v) = tricat (show k) ": " (renderJVal v)
+renderJVal (JArray a) = tricat "[" (values a) "]"
+    where values [] = ""
+          values vs = commify (map renderJVal vs)
+
+putJVal :: JValue -> IO ()
+putJVal v = putStrLn (renderJVal v)
